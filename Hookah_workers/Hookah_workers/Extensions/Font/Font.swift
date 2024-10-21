091c4f17211e60801cba4dcaa39ac8a08896245f
Karen <karen606>
2024-10-21 22:49:02 +0400

    create menu

---
+++ Hookah_workers/Hookah_workers/Extensions/Font/Font.swift
@@ -0,0 +1,31 @@
+//
+//  Font.swift
+//  Hookah_workers
+//
+//  Created by Karen Khachatryan on 21.10.24.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    static func montserratExtraBold(size: CFloat) -> UIFont? {
+        return UIFont(name: "Montserrat-ExtraBold", size: CGFloat(size))
+    }
+    
+    static func montserratBold(size: CFloat) -> UIFont? {
+        return UIFont(name: "Montserrat-Bold", size: CGFloat(size))
+    }
+    
+    static func montserratSemiBold(size: CFloat) -> UIFont? {
+        return UIFont(name: "Montserrat-SemiBold", size: CGFloat(size))
+    }
+    
+    static func montserratMedium(size: CFloat) -> UIFont? {
+        return UIFont(name: "Montserrat-Medium", size: CGFloat(size))
+    }
+    
+    static func montserratRegular(size: CFloat) -> UIFont? {
+        return UIFont(name: "Montserrat-Regular", size: CGFloat(size))
+    }
+}
---
+++ Hookah_workers/Hookah_workers/MenuViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Hookah_workers
+//
+//  Created by Karen Khachatryan on 21.10.24.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    @IBOutlet var sectionsButton: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sectionsButton.forEach({ $0.titleLabel?.font = .montserratExtraBold(size: 21) })
+    }
+
+    @IBAction func clickedMyStuffing(_ sender: UIButton) {
+//        self.pushViewController(CreatePartyViewController.self)
+    }
+    
+    @IBAction func clickedAddPadding(_ sender: UIButton) {
+//        self.pushViewController(PartiesViewController.self)
+    }
+    
+    @IBAction func clickedRating(_ sender: UIButton) {
+//        self.pushViewController(CoctailsViewController.self)
+    }
+    
+    @IBAction func clickedSetting(_ sender: UIButton) {
+//        self.pushViewController(SettingViewController.self)
+    }}
+
